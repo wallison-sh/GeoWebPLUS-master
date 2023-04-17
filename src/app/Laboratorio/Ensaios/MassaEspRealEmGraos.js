91d5a1e203909a7701b9c40fdb7ccbf4b3f283ed
wallison-sh <126890339+wallison-sh@users.noreply.github.com>
2023-04-17 15:51:01 -0300

    wa15:50

--- src/app/Laboratorio/Ensaios/MassaEspRealEmGraos.js
+++ src/app/Laboratorio/Ensaios/MassaEspRealEmGraos.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 function MassaEspRealEmGraos (){
 
-  
+
 
 
   
@@ -226,13 +226,13 @@ console.log(resul);
     <h3    >Amostra</h3>   
     </div>
     <div class="col-auto  ">
-    <input type="number"  id='amostra' name='amostra'  placeholder='Digite a amostra'  class="form-control w-90 text-center" ></input>     
+    <input   id='amostra' name='amostra'  placeholder='Digite a amostra'  class="form-control w-90 text-center" ></input>     
     </div>
     <div class="col-auto  ">
     <h3 >-</h3>   
     </div>
     <div class="col-auto  ">
-    <input id='amostra1' name='amostra1' type="number" class="form-control text-center input2 "   ></input>
+    <input id='amostra1' name='amostra1'  class="form-control text-center input2 "   ></input>
     </div>
     <div class="col-auto ">
       <button className='button '  >
@@ -322,10 +322,10 @@ console.log(resul);
     <h5 >n°</h5>
     </div>
     <div className="  col-2  espaco3  ">
-    <input name='ipicnometro1' id='ipicnometro1' type="number" className=" form-control text-center input6 "  ></input>
+    <input name='ipicnometro1' id='ipicnometro1'  className=" form-control text-center input6 "  ></input>
     </div>
     <div className="  col-2  espaco4     ">
-    <input name='ipicnometro2' id='ipicnometro2' type="number" className=" form-control text-center input6 " ></input>
+    <input name='ipicnometro2' id='ipicnometro2'  className=" form-control text-center input6 " ></input>
     </div>
 </body>
 
@@ -339,10 +339,10 @@ console.log(resul);
 <h5  > M¹  </h5>  
 </div>
 <div className="  col-2 espaco5  ">
-<input name='massumdcp1' id='massumdcp1' type="number" className=' form-control text-center input6' ></input>
+<input name='massumdcp1' id='massumdcp1' className=' form-control text-center input6' ></input>
 </div>
 <div className="  col-2 espaco6  ">
-<input name='massumdcp2' id='massumdcp2' type="number" className='form-control text-center input6' ></input>
+<input name='massumdcp2' id='massumdcp2'  className='form-control text-center input6' ></input>
 </div>
       
 </body>
@@ -359,13 +359,13 @@ console.log(resul);
 <div className="  col-2 espaco7   "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  " >
-<input name='capsula1' id='capsula1' type="number" className='form-control text-center input7' ></input>
+<input name='capsula1' id='capsula1'  className='form-control text-center input7' ></input>
 </div>
 <div className="  col-4  " >
-<input name='capsula2' id='capsula2'type="number" className='form-control  text-center input7'></input>
+<input name='capsula2' id='capsula2' className='form-control  text-center input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='capsula3' id='capsula3' type="number" className='form-control  text-center input7'></input>
+<input name='capsula3' id='capsula3'  className='form-control  text-center input7'></input>
 </div>
 </div>
 </div>
@@ -373,13 +373,13 @@ console.log(resul);
 <div className="  col-2  espaco8  "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  ">
-<input name='capsula4' id='capsula4' type="number" className='form-control  text-center input7'></input>
+<input name='capsula4' id='capsula4'  className='form-control  text-center input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='capsula5' id='capsula5' type="number" className='form-control  text-center input7'></input>
+<input name='capsula5' id='capsula5'  className='form-control  text-center input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='capsula6' id='capsula6' type="number" className=' form-control  text-center input7'></input>
+<input name='capsula6' id='capsula6' className=' form-control  text-center input7'></input>
 </div>
 </div>
 </div>
@@ -405,13 +405,13 @@ console.log(resul);
 <div className="  col-2  espaco9 "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  " >
-<input name='massolumdEcap1' id='massolumdEcap1' type="number" className='form-control  text-center input7' ></input>
+<input name='massolumdEcap1' id='massolumdEcap1'  className='form-control  text-center input7' ></input>
 </div>
 <div className="  col-4  " >
-<input name='massolumdEcap2' id='massolumdEcap2' type="number" className='form-control text-center  input7'></input>
+<input name='massolumdEcap2' id='massolumdEcap2'  className='form-control text-center  input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='massolumdEcap3' id='massolumdEcap3' type="number" className='form-control  text-center input7'></input>
+<input name='massolumdEcap3' id='massolumdEcap3'  className='form-control  text-center input7'></input>
 </div>
 </div>
 </div>
@@ -419,13 +419,13 @@ console.log(resul);
 <div className="  col-2  espaco8 "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  ">
-<input name='massolumdEcap4' id='massolumdEcap4' type="number" className='form-control text-center  input7'></input>
+<input name='massolumdEcap4' id='massolumdEcap4'  className='form-control text-center  input7'></input>
 </div>
 <div className="  col-4 ">
-<input name='massolumdEcap5' id='massolumdEcap5' type="number" className='form-control text-center  input7'></input>
+<input name='massolumdEcap5' id='massolumdEcap5'  className='form-control text-center  input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='massolumdEcap6' id='massolumdEcap6' type="number" className='form-control text-center  input7'></input>
+<input name='massolumdEcap6' id='massolumdEcap6'  className='form-control text-center  input7'></input>
 </div>
 </div>
 </div>
@@ -450,13 +450,13 @@ console.log(resul);
 <div className="  col-2 espaco9  "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  " >
-<input name='massolsecoEcap1' id='massolsecoEcap1' type="number" className='form-control text-center  input7' ></input>
+<input name='massolsecoEcap1' id='massolsecoEcap1'  className='form-control text-center  input7' ></input>
 </div>
 <div className="  col-4  " >
-<input name='massolsecoEcap2' id='massolsecoEcap2' type="number" className='form-control text-center  input7'></input>
+<input name='massolsecoEcap2' id='massolsecoEcap2'  className='form-control text-center  input7'></input>
 </div>
 <div className="  col-4 ">
-<input name='massolsecoEcap3' id='massolsecoEcap3' type="number" className='form-control text-center  input7'></input>
+<input name='massolsecoEcap3' id='massolsecoEcap3'  className='form-control text-center  input7'></input>
 </div>
 </div>
 </div>
@@ -464,13 +464,13 @@ console.log(resul);
 <div className="  col-2 espaco8  "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  ">
-<input name='massolsecoEcap4' id='massolsecoEcap4' type="number" className='form-control text-center  input7'></input>
+<input name='massolsecoEcap4' id='massolsecoEcap4'  className='form-control text-center  input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='massolsecoEcap5' id='massolsecoEcap5' type="number" className='form-control text-center  input7'></input>
+<input name='massolsecoEcap5' id='massolsecoEcap5'  className='form-control text-center  input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='massolsecoEcap6' id='massolsecoEcap6' type="number" className='form-control  text-center input7'></input>
+<input name='massolsecoEcap6' id='massolsecoEcap6'  className='form-control  text-center input7'></input>
 </div>
 </div>
 </div>
@@ -498,13 +498,13 @@ console.log(resul);
 <div className="  col-2  espaco9 "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  " >
-<input name='masscap1' id='masscap1' type="number" onBlur={TeorUmidadeCalc1} className='form-control  text-center input7' ></input>
+<input name='masscap1' id='masscap1'  onBlur={TeorUmidadeCalc1} className='form-control  text-center input7' ></input>
 </div>
 <div className="  col-4  " >
-<input name='masscap2' id='masscap2' type="number" onBlur={TeorUmidadeCalc2} className='form-control  text-center input7'></input>
+<input name='masscap2' id='masscap2'  onBlur={TeorUmidadeCalc2} className='form-control  text-center input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='masscap3' id='masscap3' type="number"  onBlur={TeorUmidadeCalc3 } className='form-control  text-center input7'></input>
+<input name='masscap3' id='masscap3'   onBlur={TeorUmidadeCalc3 } className='form-control  text-center input7'></input>
 </div>
 </div>
 </div>
@@ -512,13 +512,13 @@ console.log(resul);
 <div className="  col-2  espaco8 "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  ">
-<input name='masscap4' id='masscap4' type="number" onBlur={TeorUmidadeCalc4} className='form-control  text-center input7'></input>
+<input name='masscap4' id='masscap4'  onBlur={TeorUmidadeCalc4} className='form-control  text-center input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='masscap5' id='masscap5' type="number"  onBlur={TeorUmidadeCalc5} className='form-control  text-center input7'></input>
+<input name='masscap5' id='masscap5'   onBlur={TeorUmidadeCalc5} className='form-control  text-center input7'></input>
 </div>
 <div className="  col-4  ">
-<input name='masscap6' id='masscap6' type="number" onBlur={TeorUmidadeCalc6} className='form-control  text-center input7'></input>
+<input name='masscap6' id='masscap6'  onBlur={TeorUmidadeCalc6} className='form-control  text-center input7'></input>
 </div>
 </div>
 </div>
@@ -546,13 +546,13 @@ console.log(resul);
 <div className="  col-2 espaco9  "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  " >
-<input name='teorumd1' id='teorumd1' type="number" onBlur={TeorUmidadeMdCalc1} className='form-control  text-center input7' readonly="readonly" ></input>
+<input name='teorumd1' id='teorumd1'  onBlur={TeorUmidadeMdCalc1} className='form-control  text-center input7' readonly="readonly" ></input>
 </div>
 <div className="  col-4  " >
-<input name='teorumd2' id='teorumd2' type="number" onBlur={TeorUmidadeMdCalc1} className='form-control  text-center input7' readonly="readonly"></input>
+<input name='teorumd2' id='teorumd2'  onBlur={TeorUmidadeMdCalc1} className='form-control  text-center input7' readonly="readonly"></input>
 </div>
 <div className="  col-4  ">
-<input name='teorumd3' id='teorumd3' type="number" onBlur={TeorUmidadeMdCalc1} className='form-control  text-center input7' readonly="readonly"></input>
+<input name='teorumd3' id='teorumd3'  onBlur={TeorUmidadeMdCalc1} className='form-control  text-center input7' readonly="readonly"></input>
 </div>
 </div>
 </div>
@@ -560,13 +560,13 @@ console.log(resul);
 <div className="  col-2 espaco8  "> 
 <div className="  row  align-items-center ">
 <div className="  col-4  ">
-<input name='teorumd4' id='teorumd4' type="number" onBlur={TeorUmidadeMdCalc2} className='form-control  text-center input7' readonly="readonly"></input>
+<input name='teorumd4' id='teorumd4'  onBlur={TeorUmidadeMdCalc2} className='form-control  text-center input7' readonly="readonly"></input>
 </div>
 <div className="  col-4  ">
-<input name='teorumd5' id='teorumd5' type="number" onBlur={TeorUmidadeMdCalc2} className='form-control  text-center input7' readonly="readonly"></input>
+<input name='teorumd5' id='teorumd5'  onBlur={TeorUmidadeMdCalc2} className='form-control  text-center input7' readonly="readonly"></input>
 </div>
 <div className="  col-4  ">
-<input name='teorumd6' id='teorumd6' type="number" onBlur={TeorUmidadeMdCalc2} className='form-control  text-center input7' readonly="readonly"></input>
+<input name='teorumd6' id='teorumd6'  onBlur={TeorUmidadeMdCalc2} className='form-control  text-center input7' readonly="readonly"></input>
 </div>
 </div>
 </div>
@@ -590,10 +590,10 @@ console.log(resul);
 </div>
 
 <div className="  col-2 espaco11 ">
-<input name='teorumdMd1' id='teorumdMd1' type="number" className='form-control  text-center input6' readonly="readonly" ></input>
+<input name='teorumdMd1' id='teorumdMd1'  className='form-control  text-center input6' readonly="readonly" ></input>
 </div>
 <div className="  col-2 espaco8 ">
-<input name='teorumdMd2' id='teorumdMd2' type="number" className='form-control text-center  input6' readonly="readonly" ></input>
+<input name='teorumdMd2' id='teorumdMd2'  className='form-control text-center  input6' readonly="readonly" ></input>
 </div>
       
 </body>
@@ -616,10 +616,10 @@ console.log(resul);
 </div>
 
 <div className="  col-2 espaco11  ">
-<input name='masspicaguadt1' id='masspicaguadt1' type="number" className='form-control text-center  input6' ></input>
+<input name='masspicaguadt1' id='masspicaguadt1'  className='form-control text-center  input6' ></input>
 </div>
 <div className="  col-2 espaco8">
-<input name='masspicaguadt2' id='masspicaguadt2' type="number" className='form-control text-center  input6' ></input>
+<input name='masspicaguadt2' id='masspicaguadt2'  className='form-control text-center  input6' ></input>
 </div>
       
 </body>
@@ -642,10 +642,10 @@ console.log(resul);
 </div>
 
 <div className="  col-2 espaco11  ">
-<input name='masspicCP1' id='masspicCP1' type="number" className='form-control text-center  input6' ></input>
+<input name='masspicCP1' id='masspicCP1'  className='form-control text-center  input6' ></input>
 </div>
 <div className="  col-2 espaco8 ">
-<input name='masspicCP2' id='masspicCP2' type="number" className='form-control  text-center input6' ></input>
+<input name='masspicCP2' id='masspicCP2'  className='form-control  text-center input6' ></input>
 </div>
       
 </body>
@@ -668,10 +668,10 @@ console.log(resul);
 </div>
 
 <div className="  col-2 espaco11 ">
-<input name='TempEnsaio1' id='TempEnsaio1' type="number" className='form-control text-center  input6' ></input>
+<input name='TempEnsaio1' id='TempEnsaio1'  className='form-control text-center  input6' ></input>
 </div>
 <div className="  col-2 espaco8 ">
-<input name='TempEnsaio2' id='TempEnsaio2' type="number" className='form-control text-center  input6' ></input>
+<input name='TempEnsaio2' id='TempEnsaio2'  className='form-control text-center  input6' ></input>
 </div>
       
 </body>
@@ -694,10 +694,10 @@ console.log(resul);
 </div>
 
 <div className="  col-2 espaco11  ">
-<input name='MassEspAgaTempT1' id='MassEspAgaTempT1' type="number" onBlur={MassaEspCPCalc1} className='form-control text-center  input6' ></input>
+<input name='MassEspAgaTempT1' id='MassEspAgaTempT1'  onBlur={MassaEspCPCalc1} className='form-control text-center  input6' ></input>
 </div>
 <div className="  col-2 espaco8 ">
-<input name='MassEspAgaTempT2' id='MassEspAgaTempT2' type="number" onBlur={MassaEspCPCalc2} className='form-control text-center  input6' ></input>
+<input name='MassEspAgaTempT2' id='MassEspAgaTempT2'  onBlur={MassaEspCPCalc2} className='form-control text-center  input6' ></input>
 </div>
       
 </body>
@@ -719,10 +719,10 @@ console.log(resul);
 </div>
 
 <div className="  col-2 espaco11 ">
-<input name='massaespcppv1' id='massaespcppv1' type="number" onBlur={MassaEspMDCalc} className='form-control text-center  input6' readonly="readonly" ></input>
+<input name='massaespcppv1' id='massaespcppv1'  onBlur={MassaEspMDCalc} className='form-control text-center  input6' readonly="readonly" ></input>
 </div>
 <div className="  col-2 espaco8 ">
-<input name='massaespcppv2' id='massaespcppv2' type="number" onBlur={MassaEspMDCalc} className='form-control  text-center input6' readonly="readonly" ></input>
+<input name='massaespcppv2' id='massaespcppv2'  onBlur={MassaEspMDCalc} className='form-control  text-center input6' readonly="readonly" ></input>
 </div>
       
 </body>
@@ -745,7 +745,7 @@ console.log(resul);
 </div>
 
 <div className="  col-3 espaco11  ">
-<input name='massaespMd' id='massaespMd' type="number" className='form-control text-center  input8' readonly="readonly" ></input>
+<input name='massaespMd' id='massaespMd'  className='form-control text-center  input8' readonly="readonly" ></input>
 </div>
 
       
